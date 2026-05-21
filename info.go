0b8833431401f00b5c1f3b56ae46c181b17e9bf8
Nishant Sharma <nishant.sh230408@gmail.com>
2026-05-21 19:10:07 +0530

    getting langID from langName from GetLang slice

--- info.go
+++ info.go
@@ -1,56 +1,66 @@
 package judge0
 
-
 import (
-	"fmt"
 	"encoding/json"
-	)
-
+	"fmt"
+	"strings"
+)
 
 type Language struct {
-	ID int `json:"id"`
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
 
 type Status struct {
-	ID int `json:"id"`
+	ID          int    `json:"id"`
 	Description string `json:"description"`
-
 }
 
-//returns a slice of all supported languages by judge0
-func (c *Client) GetLanguages() ([]Language, error){
-	
-	
+// returns a slice of all supported languages by judge0
+func (c *Client) GetLanguages() ([]Language, error) {
 	data, err := c.doRequest(c.baseURL+"/languages", "GET", nil)
 
-	if err!=nil{
+	if err != nil {
 		return nil, fmt.Errorf("failed to get languages: %w", err)
 	}
 
 	var languages []Language
 	err = json.Unmarshal(data, &languages)
-	if err!=nil{
+	if err != nil {
 		return nil, fmt.Errorf("failed to serialize/Unmarshal languages: %w", err)
 	}
 
 	return languages, nil
-
 }
 
-//returns a slice of all the judge0 status codes
-func (c *Client) GetStatuses() ([]Status, error){
+// returns the Judge0 language ID for the given language name.
+func (c *Client) GetLanguageByID(name string) (int, error) {
+	languages, err := c.GetLanguages()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get languages for lookup: %w", err)
+	}
+
+	for _, lang := range languages {
+		if strings.EqualFold(lang.Name, name) {
+			return lang.ID, nil
+		}
+	}
+
+	return 0, fmt.Errorf("language %q not found", name)
+}
+
+// returns a slice of all the judge0 status codes
+func (c *Client) GetStatuses() ([]Status, error) {
 
 	data, err := c.doRequest(c.baseURL+"/statuses", "GET", nil)
 
-	if err!=nil{
+	if err != nil {
 		return nil, fmt.Errorf("failed to get statuses: %w", err)
 	}
 
-	
 	var statuses []Status
 	err = json.Unmarshal(data, &statuses)
-	if err!=nil{
+	if err != nil {
 		return nil, fmt.Errorf("failed to serialize/Unmarshal statuses: %w", err)
 	}
 
@@ -58,29 +68,21 @@ func (c *Client) GetStatuses() ([]Status, error){
 
 }
 
-
-
-
-
-
-
-
-
 const (
-	IN_QUEUE = 1
-	PROCESSING = 2
-	ACCEPTED = 3
-	WRONG_ANSWER = 4
-	TIME_LIMIT_EXCEEDED = 5
-	COMPILATION_ERROR = 6
+	IN_QUEUE              = 1
+	PROCESSING            = 2
+	ACCEPTED              = 3
+	WRONG_ANSWER          = 4
+	TIME_LIMIT_EXCEEDED   = 5
+	COMPILATION_ERROR     = 6
 	RUNTIME_ERROR_SIGSEGV = 7
 	RUNTIME_ERROR_SIGXFSZ = 8
-	RUNTIME_ERROR_SIGFPE = 9
+	RUNTIME_ERROR_SIGFPE  = 9
 	RUNTIME_ERROR_SIGABRT = 10
-	RUNTIME_ERROR_NZEC = 11
-	RUNTIME_ERROR_OTHER = 12
-	INTERNAL_ERROR = 13
-	EXEC_FORMAT_ERROR = 14
+	RUNTIME_ERROR_NZEC    = 11
+	RUNTIME_ERROR_OTHER   = 12
+	INTERNAL_ERROR        = 13
+	EXEC_FORMAT_ERROR     = 14
 )
 
 var statusMap = map[int]string{
@@ -99,4 +101,3 @@ var statusMap = map[int]string{
 	INTERNAL_ERROR:        "INTERNAL_ERROR",
 	EXEC_FORMAT_ERROR:     "EXEC_FORMAT_ERROR",
 }
-
